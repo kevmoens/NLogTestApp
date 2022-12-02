eb78619352fe2509f400662ca7dda31cca028c34
Kevin <kmoens@lakeco.com>
2022-12-02 13:52:41 -0600

    Add project files.

---
+++ NLogTestApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLogTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
